91c77a812da315570512aeb00d9e4d616c83d104
Sandy <78834828+Ganji-Sandeep-10@users.noreply.github.com>
2025-10-02 17:09:42 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,30 +1,28 @@
-# This is a dummy Python file - modified version
+# This is an updated Python file
 
 # Arithmetic operation
-a = 7   # changed from 5
-b = 12  # changed from 10
-sum_ab = a + b
-print(f'Sum of a and b is: {sum_ab}')
+a = 15
+b = 20
+sum_ab = a * b  # Changed from addition to multiplication
+print(f'Product of a and b is: {sum_ab}')
 
 # List operations
-my_list = [1, 2, 3, 4, 5]
-my_list.append(6)
-my_list.append(7)  # added new element
-print(f'Updated list: {my_list}')
+my_list = [10, 9, 8, 7, 6]
+my_list.insert(0, 5)  # Changed to insert at the beginning
+print(f'Updated reversed list: {my_list}')
 
 # Conditional
-if a < b:
-    print('a is less than b')
+if a > b:
+    print('a is greater than b')
 else:
-    print('a is greater than or equal to b')  # new branch
+    print('a is not greater than b')  # Added else branch
 
 # Loop
-for i in range(4):  # changed range from 3 to 4
-    print(f'Loop iteration: {i}')
+for i in range(1, 4):  # Changed loop range
+    print(f'Updated loop iteration: {i}')
 
 # Function
 def greet(name):
-    print(f'Hello, {name}!')
+    print(f'Hello and welcome, {name}!')  # Modified greeting
 
-greet('User')
-greet('Developer')  # added an extra call
+greet('Developer')
