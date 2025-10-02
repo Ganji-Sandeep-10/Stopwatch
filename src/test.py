57442ae728433ce32211867f087435ff36cf2d24
Sandy <78834828+Ganji-Sandeep-10@users.noreply.github.com>
2025-10-02 17:03:38 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,22 +1,25 @@
-# This is a dummy Python file
+# This is a dummy Python file - modified version
 
 # Arithmetic operation
-a = 5
-b = 10
+a = 7   # changed from 5
+b = 12  # changed from 10
 sum_ab = a + b
 print(f'Sum of a and b is: {sum_ab}')
 
 # List operations
 my_list = [1, 2, 3, 4, 5]
 my_list.append(6)
+my_list.append(7)  # added new element
 print(f'Updated list: {my_list}')
 
 # Conditional
 if a < b:
     print('a is less than b')
+else:
+    print('a is greater than or equal to b')  # new branch
 
 # Loop
-for i in range(3):
+for i in range(4):  # changed range from 3 to 4
     print(f'Loop iteration: {i}')
 
 # Function
@@ -24,3 +27,4 @@ def greet(name):
     print(f'Hello, {name}!')
 
 greet('User')
+greet('Developer')  # added an extra call
