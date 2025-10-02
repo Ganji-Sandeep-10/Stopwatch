c018d032cfc93a1e6a00713bde8187eea06f856d
Sandy <78834828+Ganji-Sandeep-10@users.noreply.github.com>
2025-10-02 14:12:50 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,3 +1,9 @@
+num1 = 10
+num2 = 20
+sum = num1 + num2
+print(f"The sum of {num1} and {num2} is: {sum}")
+
+
 my_list = [1, 2, 3, 4, 5]
 my_list.append(6)  # Add an element
 print(f"List after appending: {my_list}")
