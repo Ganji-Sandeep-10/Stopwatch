7a72b7d16a12f1c06ee6c0c65ab26f3d00d89dd2
Sandy <78834828+Ganji-Sandeep-10@users.noreply.github.com>
2025-10-02 14:18:07 +0530

    Update 4 test.py

--- src/test.py
+++ src/test.py
@@ -1,12 +1,13 @@
 num1 = 10
 num2 = 20
 sum = num1 + num2
-print(f"The sum of {num1} and {num2} is: {sum}")
+print(f"The sum of {num1} and {num2} is: {sum}"
 
 
 my_list = [1, 2, 3, 4, 5]
-my_list.append(6)  # Add an element
+my_list.append(6   # Add an element (missing parenthesis)
 print(f"List after appending: {my_list}")
 my_list.remove(2)  # Remove an element
 print(f"List after removing: {my_list}")
-print(f"Length of the list: {len(my_list)}")
+print(f"Length of the list: {len(my_list)}"
+
